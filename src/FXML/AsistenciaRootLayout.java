73168c8deda99314f293a873dd4e5d3eaa46937d
pepsypollo <pepe.esp.ort@gmail.com>
2017-11-08 13:46:20 +0100

    Controllers

---
+++ src/FXML/AsistenciaRootLayout.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FXML;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+/**
+ * FXML Controller class
+ *
+ * @author ITP1DAM
+ */
+public class AsistenciaRootLayout implements Initializable {
+
+    @FXML
+    private MenuItem stackedBarChart;
+    @FXML
+    private MenuItem piechart;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void handleNew(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleOpen(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleSave(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleSaveAs(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleExit(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleShowBirthdayStatistics(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleBarchar(ActionEvent event) {
+    }
+
+    @FXML
+    private void handlePieChart(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleAbout(ActionEvent event) {
+    }
+    
+}
