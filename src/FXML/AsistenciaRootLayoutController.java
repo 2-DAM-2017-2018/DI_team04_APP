83adfc5ebc189dcf13637adb433268c1fc7704a1
pepsypollo <pepe.esp.ort@gmail.com>
2017-11-08 14:16:36 +0100

    MainApp init + fixes

---
+++ src/FXML/AsistenciaRootLayoutController.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FXML;
+
+import Main.MainApp;
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+import javafx.stage.FileChooser;
+
+/**
+ * FXML Controller class
+ *
+ * @author ITP1DAM
+ */
+public class AsistenciaRootLayoutController {
+
+    private MainApp mainApp;
+
+    /**
+     * Is called by the main application to give a reference back to itself.
+     * 
+     * @param mainApp
+     */
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+    }
+
+    /**
+     * Creates an empty address book.
+     */
+    @FXML
+    private void handleNew() {
+        mainApp.getAlumnData().clear();
+        mainApp.setAlumnFilePath(null);
+    }
+
+    /**
+     * Opens a FileChooser to let the user select an address book to load.
+     */
+    @FXML
+    private void handleOpen() {
+        FileChooser fileChooser = new FileChooser();
+
+        // Set extension filter
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(
+                "XML files (*.xml)", "*.xml");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        // Show save file dialog
+        File file = fileChooser.showOpenDialog(mainApp.getPrimaryStage());
+
+        if (file != null) {
+            mainApp.loadAlumnDataFromFile(file);
+        }
+    }
+
+    /**
+     * Saves the file to the person file that is currently open. If there is no
+     * open file, the "save as" dialog is shown.
+     */
+    @FXML
+    private void handleSave() {
+        File personFile = mainApp.getAlumnFilePath();
+        if (personFile != null) {
+            mainApp.saveAlumnDataToFile(personFile);
+        } else {
+            handleSaveAs();
+        }
+    }
+
+    /**
+     * Opens a FileChooser to let the user select a file to save to.
+     */
+    @FXML
+    private void handleSaveAs() {
+        FileChooser fileChooser = new FileChooser();
+
+        // Set extension filter
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(
+                "XML files (*.xml)", "*.xml");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        // Show save file dialog
+        File file = fileChooser.showSaveDialog(mainApp.getPrimaryStage());
+
+        if (file != null) {
+            // Make sure it has the correct extension
+            if (!file.getPath().endsWith(".xml")) {
+                file = new File(file.getPath() + ".xml");
+            }
+            mainApp.saveAlumnDataToFile(file);
+        }
+    }
+
+    /**
+     * Opens an about dialog.
+     */
+    @FXML
+    private void handleAbout() {
+//        Dialogs.create()
+//            .title("AddressApp")
+//            .masthead("About")
+//            .message("Author: Marco Jakob\nWebsite: http://code.makery.ch")
+//            .showInformation();
+    }
+
+    /**
+     * Closes the application.
+     */
+    @FXML
+    private void handleExit() {
+        System.exit(0);
+    }
+    
+    /**
+    * Opens the birthday statistics.
+    */
+}
---
+++ src/Main/MainApp.java
@@ -0,0 +1,283 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import Main.Alumno;
+//import FXML.makery.address.model.PersonListWrapper;
+import FXML.AñadirAlumnoController;
+import FXML.ListaAlumnosController;
+import FXML.AsistenciaRootLayoutController;
+import java.io.File;
+import java.io.IOException;
+import java.util.prefs.Preferences;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+/**
+ *
+ * @author franciscogonzalezdelvalle
+ */
+public class MainApp extends Application {
+  
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    /**
+     * The data as an observable list of Persons.
+     */
+    private ObservableList<Alumno> alumnData = FXCollections.observableArrayList();
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AddressApp");
+        
+        // set the application icon
+        this.primaryStage.getIcons().add(new Image("file:resources/images/MailIcon.png"));
+
+        initRootLayout();
+
+        showAlumnOverview();
+    }
+
+    /**
+     * Constructor
+     */
+    public MainApp() {
+        // Add some sample data
+        alumnData.add(new Alumno("Hans", "Muster", "1A"));
+        alumnData.add(new Alumno("Ruth", "Mueller", "1A"));
+        alumnData.add(new Alumno("Heinz", "Kurz", "1B"));
+        alumnData.add(new Alumno("Cornelia", "Meier", "1B"));
+        alumnData.add(new Alumno("Werner", "Meyer", "2A"));
+        alumnData.add(new Alumno("Lydia", "Kunz", "2A"));
+        alumnData.add(new Alumno("Anna", "Best", "2B"));
+        alumnData.add(new Alumno("Stefan", "Meier", "2B"));
+    }
+
+    /**
+     * Returns the data as an observable list of Persons. 
+     * @return
+     */
+    public ObservableList<Alumno> getAlumnData() {
+        return alumnData;
+    }
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+             // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class
+                    .getResource("FXML/AsistenciaRootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+
+            // Give the controller access to the main app.
+            AsistenciaRootLayoutController controller = loader.getController();
+            controller.setMainApp(this);
+
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Try to load last opened person file.
+        File file = getAlumnFilePath();
+        if (file != null) {
+            loadAlumnDataFromFile(file);
+        }
+    }
+    /**
+     * Shows the person overview inside the root layout.
+     */
+    public void showAlumnOverview() {
+        try {
+        // Load person overview.
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
+        AnchorPane personOverview = (AnchorPane) loader.load();
+
+        // Set person overview into the center of root layout.
+        rootLayout.setCenter(personOverview);
+
+        // Give the controller access to the main app.
+        ListaAlumnosController controller = loader.getController();
+        controller.setMainApp(this);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Opens a dialog to edit details for the specified person. If the user
+     * clicks OK, the changes are saved into the provided person object and true
+     * is returned.
+     * 
+     * @param person the person object to be edited
+     * @return true if the user clicked OK, false otherwise.
+     */
+    public boolean showAlumnEditDialog(Alumno alumn) {
+        try {
+            // Load the fxml file and create a new stage for the popup dialog.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/PersonEditDialog.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+
+            // Create the dialog Stage.
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Edit Person");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
+
+            // Set the person into the controller.
+            AñadirAlumnoController controller = loader.getController();
+            controller.setDialogStage(dialogStage);
+            controller.setPerson(alumn);
+
+            // Set the dialog icon.
+    //        dialogStage.getIcons().add(new Image("file:resources/images/edit.png"));
+
+            // Show the dialog and wait until the user closes it
+            dialogStage.showAndWait();
+
+            return controller.isOkClicked();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /**
+     * Returns the person file preference, i.e. the file that was last opened.
+     * The preference is read from the OS specific registry. If no such
+     * preference can be found, null is returned.
+     * 
+     * @return
+     */
+    public File getAlumnFilePath() {
+        Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+        String filePath = prefs.get("filePath", null);
+        if (filePath != null) {
+            return new File(filePath);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Sets the file path of the currently loaded file. The path is persisted in
+     * the OS specific registry.
+     * 
+     * @param file the file or null to remove the path
+     */
+    public void setAlumnFilePath(File file) {
+        Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+        if (file != null) {
+            prefs.put("filePath", file.getPath());
+
+            // Update the stage title.
+            primaryStage.setTitle("AddressApp - " + file.getName());
+        } else {
+            prefs.remove("filePath");
+
+            // Update the stage title.
+            primaryStage.setTitle("AddressApp");
+        }
+    } 
+
+    /**
+    * Loads person data from the specified file. The current person data will
+    * be replaced.
+    * 
+    * @param file
+    */
+   public void loadAlumnDataFromFile(File file) {
+       try {
+           JAXBContext context = JAXBContext
+                   .newInstance(PersonListWrapper.class);
+           Unmarshaller um = context.createUnmarshaller();
+
+           // Reading XML from the file and unmarshalling.
+           PersonListWrapper wrapper = (PersonListWrapper) um.unmarshal(file);
+
+           alumnData.clear();
+           alumnData.addAll(wrapper.getPersons());
+
+           // Save the file path to the registry.
+           setAlumnFilePath(file);
+
+       } catch (Exception e) { // catches ANY exception
+   //        Dialogs.create()
+   //                .title("Error")
+   //                .masthead("Could not load data from file:\n" + file.getPath())
+   //                .showException(e);
+       }
+    }
+
+    /**
+     * Saves the current person data to the specified file.
+     * 
+     * @param file
+     */
+    public void saveAlumnDataToFile(File file) {
+        try {
+            JAXBContext context = JAXBContext
+                    .newInstance(PersonListWrapper.class);
+            Marshaller m = context.createMarshaller();
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            // Wrapping our person data.
+            PersonListWrapper wrapper = new PersonListWrapper();
+            wrapper.setPersons(alumnData);
+
+            // Marshalling and saving XML to the file.
+            m.marshal(wrapper, file);
+
+            // Save the file path to the registry.
+            setAlumnFilePath(file);
+        } catch (Exception e) { // catches ANY exception
+    //                Dialogs.create().title("Error")
+    //                .masthead("Could not save data to file:\n" + file.getPath())
+    //                .showException(e);
+        }
+    }
+    
+}
